d27d7024e790e56fbc6fcc714a6b5295e23f81d2
Mico Ocampo <meaocampo@Micos-MacBook-Pro.local>
2025-06-30 12:54:54 -0700

    fix: updating distPath

--- server/vite.ts
+++ server/vite.ts
@@ -81,7 +81,7 @@ export function serveStatic(app: Express) {
   // catch-all route to serve the client app
   app.get("*", (req, res) => {
     res.sendFile(
-      path.resolve(__dirname, "../client/dist/index.html"),
+      path.resolve(__dirname, "index.html"),
     );
   });
 }
