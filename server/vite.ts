497d7aa513423cff397f5630e78dc10a1d89fbf1
Mico Ocampo <meaocampo@Micos-MacBook-Pro.local>
2025-06-30 12:22:59 -0700

    fix: fixing page reload issue

--- server/vite.ts
+++ server/vite.ts
@@ -78,8 +78,10 @@ export function serveStatic(app: Express) {
 
   app.use(express.static(distPath));
 
-  // fall through to index.html if the file doesn't exist
-  app.use("*", (_req, res) => {
-    res.sendFile(path.resolve(distPath, "index.html"));
+  // catch-all route to serve the client app
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "../client/dist/index.html"),
+    );
   });
 }
